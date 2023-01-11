6bea06ddee09a0cac6c3b634ac16539eb2a14a92
DavidBalbino <debarbino@gmail.com>
2023-01-11 08:13:44 -0300

    fazendo as primeiras requisicoes

---
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { IPaginacao } from '../../interfaces/IPaginacao';
+import IRestaurante from '../../interfaces/IRestaurante';
+import style from './ListaRestaurantes.module.scss';
+import Restaurante from './Restaurante';
+
+const ListaRestaurantes = () => {
+
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+  const [proximaPagina, setProximaPagina] = useState('')
+
+  useEffect(() => {
+    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
+    .then(resposta => {
+      console.log(resposta)
+      setRestaurantes(resposta.data.results)
+      setProximaPagina(resposta.data.next)
+    }).catch(erro => {
+      console.log(erro)
+    })
+  },[])
+
+  return (<section className={style.ListaRestaurantes}>
+    <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+    {proximaPagina && 
+      <button>
+        ver mais
+      </button>}
+  </section>)
+}
+
+export default ListaRestaurantes
